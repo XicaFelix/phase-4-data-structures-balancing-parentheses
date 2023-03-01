b09667d906b5624751425604e949e6377d0e97c9
Xica Felix <xicasfelix@gmail.com>
2023-03-01 16:49:08 -0500

    commit tree 3310a81600071c6e54841711ddeaef5ea3b7779f

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(str)
+    stack = Stack.new
+
+    str.chars.each do |char|
+        if stack.peek == '(' and char == ')'
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+    return stack.size
+end
+
+
